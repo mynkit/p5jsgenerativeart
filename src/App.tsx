36c608e826a3585724cb43e8c79714ada368aa19
mynkit <dcm5124289@gmail.com>
2023-02-14 15:58:09 +0900

    停止と再開できるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import {
   P5CanvasInstance,
   ReactP5Wrapper,
@@ -6,39 +7,48 @@ import {
   Sketch
 } from "react-p5-wrapper";
 import Grid from '@mui/material/Grid';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
 
 type MySketchProps = SketchProps & {
   penSpeed: number;
+  pause: boolean;
 };
 
 const App = () => {
   const [penSpeed, setPenSpeed] = useState(3);
+  const [pause, setPause] = useState(true);
   
   return (
     <div className="App">
-      <Grid style={{position: 'fixed', width: '500px', maxWidth: '100%', height: '100vh'}}>
-        <Grid container alignItems={'center'} style={{padding: '10px', textAlign: 'center', position: 'absolute', bottom: '0px'}}>
-        <Grid item xs={12-3.5} />
-          <Grid item xs={1} style={{textAlign: 'center'}} onClick={()=>{
+      <Grid style={{position: 'fixed', width: '100%', maxWidth: '100%', height: '100%'}}>
+        {/* 再生停止制御 */}
+        <Grid container alignItems={'center'} style={{padding: '15px', textAlign: 'center', position: 'absolute', bottom: '0px', left: '0px', width: '0', zIndex: '10', marginBottom: '20px'}}>
+          <Grid item xs={1} alignItems={'center'} style={{textAlign: 'center', cursor: 'pointer'}} onClick={()=>{setPause(v=>!v)}}>
+            {pause ? <PlayArrowIcon fontSize="large"/> : <PauseIcon fontSize="large"/>}
+          </Grid>
+        </Grid>
+        {/* 処理速度変更 */}
+        <Grid container alignItems={'center'} style={{padding: '15px', textAlign: 'center', position: 'absolute', bottom: '0px', right: '0px', width: '750px', maxWidth: '100%', marginBottom: '25px'}}>
+          <Grid item xs={12-4.5} />
+          <Grid item xs={1} style={{textAlign: 'center', cursor: 'pointer'}} onClick={()=>{
             if (penSpeed>1) {
               setPenSpeed(v=>v-0.5);
             }
           }}>
-            <ArrowBackIcon/>
+            <p style={{fontSize: '15pt', margin: '0px'}}>{'◁'}</p>
           </Grid>
-          <Grid item xs={1.5} alignItems={'center'}>
-            <p style={{fontSize: '15pt'}}>{penSpeed}</p>
+          <Grid item xs={2.5} style={{textAlign: 'center'}} alignItems={'center'}>
+            <p style={{fontSize: '20pt', margin: '0px'}}>{penSpeed}</p>
           </Grid>
-          <Grid item xs={1} alignItems={'center'} onClick={()=>{
+          <Grid item xs={1} style={{textAlign: 'center', cursor: 'pointer'}} alignItems={'center'} onClick={()=>{
             setPenSpeed(v=>v+0.5);
           }}>
-            <ArrowForwardIcon/>
+            <p style={{fontSize: '15pt', margin: '0px'}}>{'▷'}</p>
           </Grid>
         </Grid>
       </Grid>
-      <ReactP5Wrapper sketch={sketch} penSpeed={penSpeed}></ReactP5Wrapper>
+      <ReactP5Wrapper sketch={sketch} penSpeed={penSpeed} pause={pause}></ReactP5Wrapper>
     </div>
   );
 }
@@ -54,6 +64,7 @@ const sketch: Sketch<MySketchProps> = (p: P5CanvasInstance<MySketchProps>) => {
   let time: number = 0;
   let penSpeed: number = 2;
   let sizeTras: number = p.min(width, height) / 1000;
+  let pause: boolean = true;
 
   p.setup = () => {
     p.createCanvas(width, height);
@@ -65,9 +76,14 @@ const sketch: Sketch<MySketchProps> = (p: P5CanvasInstance<MySketchProps>) => {
     if (props.penSpeed) {
       penSpeed = props.penSpeed;
     }
+    if (props.pause!==undefined) {
+      pause = props.pause;
+    }
   };
 
   p.draw = () => {
+    if (pause) {return;}
+
     let centerX, centerY;
     let newX, newY;
 
