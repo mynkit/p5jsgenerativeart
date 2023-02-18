573cab75bc1eda308a3ad2887d3399b149b0313f
mynkit <dcm5124289@gmail.com>
2023-02-18 12:39:22 +0900

    スタイル調整+情報追加

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -17,12 +17,18 @@ const Home: React.FC<HomeProps> = ({ setTarget }) => {
   }
   return (
     <div style={{padding: '17px'}}>
-      <Grid container style={{height: '60px', textAlign: 'left'}}>
-        <p style={{fontSize: '16pt', margin: '0', padding: '0', height: '100%', textAlign: 'center'}}>Generative Art</p>
+      <p style={{fontSize: '16pt', margin: '0', padding: '0', height: '100%', textAlign: 'left'}}>Generative Art</p>
+      <Grid container justifyContent='left' alignItems='center' style={{columnGap: '0px'}}>
+        <p style={{fontSize: '10pt', margin: '0', paddingTop: '10px', paddingBottom: '10px', height: '100%', textAlign: 'left'}}>
+          made by
+        </p>
+        <a href='https://keita-miyano.async-studio.com/' target="_blank" rel="noopener noreferrer" style={{height: '30px'}}>
+          <img src='/logo/logo_keitamiyano_rgb_1-5.png' height='100%'></img>
+        </a>
       </Grid>
       <Grid container justifyContent='center' style={{columnGap: '20px', rowGap: '5px'}}>
-        <img style={{width: '800px', maxWidth: '60%', cursor: 'pointer'}} src={`/thumb/plankton1-${randint(1,2)}.png`} alt="plankton1" onClick={()=>imgClick('plankton1')}/>
-        <img style={{width: '800px', maxWidth: '60%', cursor: 'pointer'}} src={`/thumb/plankton2-${randint(1,2)}.png`} alt="plankton2" onClick={()=>imgClick('plankton2')}/>
+        <img style={{width: '600px', maxWidth: '60%', cursor: 'pointer'}} src={`/thumb/plankton1-${randint(1,2)}.png`} alt="plankton1" onClick={()=>imgClick('plankton1')}/>
+        <img style={{width: '600px', maxWidth: '60%', cursor: 'pointer'}} src={`/thumb/plankton2-${randint(1,2)}.png`} alt="plankton2" onClick={()=>imgClick('plankton2')}/>
       </Grid>
     </div>
   )
